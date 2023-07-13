e86ff9fe4293eea7e0ad4ab5aac913ea6b5f7038
Jeb Bearer <jeb@espressosys.com>
2023-07-13 15:49:21 -0700

    Add end-to-end test for preconfirmations
    The test sets up an L2 system with a regular node and a
    preconfirmations node. It sequences a transaction, checks that
    both nodes finalize the transaction, and asserts that the
    preconfirmation node finalized it first.
    
    With a HotShot block time of 1s and L1 block time of 5s, this
    should be highly deterministic. Indeed, on my machine, the
    preconfirmation arrives 5-10s before the final confirmation.

--- polygon-zkevm-adaptor/tests/end_to_end.rs
+++ polygon-zkevm-adaptor/tests/end_to_end.rs
@@ -1,32 +1,25 @@
 use async_compatibility_layer::logging::{setup_backtrace, setup_logging};
-use async_std::task::{sleep, spawn_local};
+use async_std::task::{sleep, spawn};
 use ethers::{prelude::*, providers::Middleware};
 use futures::{
     future::{ready, FutureExt},
     join,
-    stream::StreamExt,
+    stream::{StreamExt, TryStream, TryStreamExt},
 };
 use hotshot_query_service::availability::BlockQueryData;
 use polygon_zkevm_adaptor::{Layer1Backend, ZkEvmNode};
 use sequencer::hotshot_commitment::{run_hotshot_commitment_task, CommitmentTaskOptions};
 use sequencer::SeqTypes;
 use sequencer_utils::{connect_rpc, wait_for_http};
-use std::time::Duration;
+use std::fmt::Debug;
+use std::time::{Duration, Instant};
 use tempfile::TempDir;
 use zkevm::ZkEvm;
 use zkevm_contract_bindings::PolygonZkEVM;
 
 #[async_std::test]
 async fn test_end_to_end() {
-    setup_logging();
-    setup_backtrace();
-
-    let node = ZkEvmNode::start("test-end-to-end".to_string(), Layer1Backend::Anvil).await;
-
-    // Create blocks periodically. This seems to be required, but we should
-    // investigate how exactly the Ethereum block number drivers the
-    // zkevm-node.
-    node.l1().mine_blocks_periodic(Duration::from_secs(1)).await;
+    let (node, _stores) = setup_test("test-end-to-end", Duration::from_secs(1)).await;
 
     // Get test setup from environment.
     let env = node.env();
@@ -34,7 +27,6 @@ async fn test_end_to_end() {
     let l2_provider = env.l2_provider();
     let mnemonic = env.funded_mnemonic();
     let rollup_address = node.l1().rollup.address();
-    let hotshot_address = node.l1().hotshot.address();
 
     let l1 = connect_rpc(&l1_provider, mnemonic, 0, None).await.unwrap();
     let l2 = connect_rpc(&l2_provider, mnemonic, 0, None).await.unwrap();
@@ -44,56 +36,6 @@ async fn test_end_to_end() {
     let rollup = PolygonZkEVM::new(rollup_address, l1.clone());
     let l1_initial_block = l1.get_block_number().await.unwrap();
     let l2_initial_balance = l2.get_balance(l2.inner().address(), None).await.unwrap();
-    tracing::info!(
-        "address: {}, rollup address: {}, hotshot address: {}, L1 initial block: {}, \
-        L2 initial balance: {}",
-        l1.inner().address(),
-        rollup.address(),
-        hotshot_address,
-        l1_initial_block,
-        l2_initial_balance,
-    );
-
-    // Start a sequencer network.
-    let nodes = sequencer::testing::init_hotshot_handles().await;
-    let api_node = nodes[0].clone();
-    let sequencer_store = TempDir::new().unwrap();
-    let options = sequencer::api::Options {
-        port: env.sequencer_port(),
-        storage_path: sequencer_store.path().into(),
-        reset_store: true,
-    };
-
-    sequencer::api::serve(options, Box::new(move |_| ready((api_node, 0)).boxed()))
-        .await
-        .unwrap();
-    for node in nodes {
-        node.start().await;
-    }
-
-    // Start a Polygon zkEVM adaptor.
-    let adaptor_opt = polygon_zkevm_adaptor::Options {
-        l1_provider: env.l1_provider(),
-        sequencer_url: env.sequencer(),
-        rpc_port: env.l2_adaptor_rpc_port(),
-        l2_chain_id: zkevm.chain_id,
-        query_port: env.l2_adaptor_query_port(),
-    };
-    let hotshot_contract_opt = CommitmentTaskOptions {
-        l1_provider: env.l1_provider(),
-        sequencer_mnemonic: mnemonic.to_string(),
-        sequencer_account_index: node.l1().clients.funded[0].index,
-        hotshot_address,
-        l1_chain_id: None,
-        query_service_url: Some(env.sequencer()),
-    };
-    spawn_local(async move {
-        join!(
-            polygon_zkevm_adaptor::json_rpc::serve(&adaptor_opt),
-            polygon_zkevm_adaptor::query_service::serve(&adaptor_opt),
-            run_hotshot_commitment_task(&hotshot_contract_opt)
-        );
-    });
 
     // Subscribe to a block stream so we can find the blocks that end up including our transactions.
     tracing::info!("connecting to sequencer at {}", env.sequencer());
@@ -163,42 +105,15 @@ async fn test_end_to_end() {
 
         // Wait for the transaction to be included in a block. We must ensure this transaction is
         // sequenced before the next one, or both could be invalidated due to nonce misordering.
-        let block_num = 'block: loop {
-            let block: BlockQueryData<SeqTypes> = blocks.next().await.unwrap().unwrap();
-            tracing::info!("got block {:?}", block);
-            for txn in zkevm.vm_transactions(block.block()) {
-                let sequenced_hash = txn.hash();
-                if sequenced_hash == hash {
-                    tracing::info!("transaction {} sequenced", i);
-                    break 'block block.height();
-                } else {
-                    tracing::info!("unknown transaction {} sequenced", sequenced_hash);
-                }
-            }
-        };
+        let block_num = wait_for_block_containing_txn(&mut blocks, zkevm, hash).await;
 
         txn_hashes.push(hash);
         block_nums.push(block_num);
     }
 
-    // Wait for the transactions to complete on L2. Note that awaiting a [PendingTransaction]
-    // will not work here -- [PendingTransaction] returns [None] if the transaction is thrown
-    // out of the mempool, but since we bypassed the sequencer, our transactions were never in
-    // the mempool in the first place.
-    for (i, hash) in txn_hashes.into_iter().enumerate() {
-        loop {
-            if let Some(receipt) = l2.get_transaction_receipt(hash).await.unwrap() {
-                tracing::info!("transfer {} completed: {:?}", i, receipt);
-                break;
-            }
-            tracing::info!("Waiting for transfer {} to complete", i);
-            tracing::info!(
-                "L2 balance {}/{}",
-                l2.get_balance(l2.inner().address(), None).await.unwrap(),
-                l2_initial_balance
-            );
-            sleep(Duration::from_secs(1)).await;
-        }
+    // Wait for the transactions to complete on L2.
+    for hash in txn_hashes {
+        await_transaction(&l2, hash).await;
     }
 
     // Check the effects of the transfers.
@@ -231,4 +146,196 @@ async fn test_end_to_end() {
         .await
         .unwrap()
         .is_none());
+}
+
+#[async_std::test]
+async fn test_preconfirmations() {
+    setup_logging();
+    setup_backtrace();
+
+    let (node, _stores) = setup_test("test-preconfirmations", Duration::from_secs(5)).await;
+    let env = node.env();
+    let mnemonic = env.funded_mnemonic();
+    let l2 = connect_rpc(&env.l2_provider(), mnemonic, 0, None)
+        .await
+        .unwrap();
+    let l2_preconf = connect_rpc(&env.l2_preconfirmations_provider(), mnemonic, 0, None)
+        .await
+        .unwrap();
+    let zkevm = ZkEvm {
+        chain_id: l2.get_chainid().await.unwrap().as_u64(),
+    };
+    let l2_initial_balance = l2.get_balance(l2.inner().address(), None).await.unwrap();
+
+    // Subscribe to a block stream so we can find the block that ends up including our transaction.
+    tracing::info!("connecting to sequencer at {}", env.sequencer());
+    let sequencer = surf_disco::Client::<hotshot_query_service::Error>::new(env.sequencer());
+    sequencer.connect(None).await;
+    let mut blocks = sequencer
+        .socket("availability/stream/blocks/0")
+        .subscribe()
+        .await
+        .unwrap();
+
+    // Wait for the adaptor to start serving.
+    tracing::info!("connecting to adaptor RPC at {}", env.l2_adaptor_rpc());
+    // The adaptor is not a full RPC, therefore we can't use `wait_for_rpc`.`
+    wait_for_http(&env.l2_adaptor_rpc(), Duration::from_secs(1), 100)
+        .await
+        .unwrap();
+    tracing::info!(
+        "connecting to adaptor query service at {}",
+        env.l2_adaptor_query()
+    );
+    wait_for_http(&env.l2_adaptor_query(), Duration::from_secs(1), 100)
+        .await
+        .unwrap();
+
+    // Create a test transaction.
+    let transfer_amount = 1.into();
+    let txn_hash = l2
+        .send_transaction(
+            TransactionRequest {
+                from: Some(l2.inner().address()),
+                to: Some(Address::zero().into()),
+                value: Some(transfer_amount),
+                ..Default::default()
+            },
+            None,
+        )
+        .await
+        .unwrap()
+        .tx_hash();
+    tracing::info!("Sent transaction {:?}", txn_hash);
+
+    // Wait for the transaction to be included in a block.
+    wait_for_block_containing_txn(&mut blocks, zkevm, txn_hash).await;
+    tracing::info!("Transaction sequenced at {:?}", Instant::now());
+
+    // Wait for the transaction to complete on L2, using both the regular RPC and the
+    // preconfirmation RPC in parallel.
+    let (pre_conf, slow_conf) = join!(
+        await_transaction(&l2_preconf, txn_hash),
+        await_transaction(&l2, txn_hash),
+    );
+
+    // Check the effects of the transfer.
+    assert_eq!(
+        l2.get_balance(l2.inner().address(), None).await.unwrap(),
+        l2_initial_balance - transfer_amount
+    );
+    assert_eq!(
+        l2_preconf
+            .get_balance(l2_preconf.inner().address(), None)
+            .await
+            .unwrap(),
+        l2_initial_balance - transfer_amount
+    );
+
+    // Check that we got the preconfirmation first.
+    tracing::info!(
+        "preconfirmation received at {pre_conf:?}, final confirmation received at {slow_conf:?}"
+    );
+    assert!(pre_conf < slow_conf);
+}
+
+async fn wait_for_block_containing_txn<B>(mut blocks: B, zkevm: ZkEvm, hash: H256) -> u64
+where
+    B: TryStream<Ok = BlockQueryData<SeqTypes>> + Unpin,
+    B::Error: Debug,
+{
+    loop {
+        let block = blocks.try_next().await.unwrap().unwrap();
+        tracing::info!("got block {:?}", block);
+        for txn in zkevm.vm_transactions(block.block()) {
+            let sequenced_hash = txn.hash();
+            if sequenced_hash == hash {
+                tracing::info!("transaction {hash} sequenced");
+                return block.height();
+            } else {
+                tracing::info!("unknown transaction {sequenced_hash} sequenced");
+            }
+        }
+    }
+}
+
+async fn await_transaction(rpc: &impl Middleware, hash: H256) -> Instant {
+    // Note that awaiting a [PendingTransaction] will not work here -- [PendingTransaction] returns
+    // [None] if the transaction is thrown out of the mempool, but since we bypassed the sequencer,
+    // our transactions were never in the mempool in the first place.
+    loop {
+        if let Some(receipt) = rpc.get_transaction_receipt(hash).await.unwrap() {
+            tracing::info!("transfer {hash} completed: {receipt:?}");
+            break;
+        }
+        tracing::info!("Waiting for transfer {hash} to complete");
+        sleep(Duration::from_secs(1)).await;
+    }
+    Instant::now()
+}
+
+async fn setup_test(name: &str, l1_block_time: Duration) -> (ZkEvmNode, TempDir) {
+    setup_logging();
+    setup_backtrace();
+
+    let node = ZkEvmNode::start(name.to_string(), Layer1Backend::Anvil).await;
+
+    // Create blocks periodically. This seems to be required, but we should
+    // investigate how exactly the Ethereum block number drivers the
+    // zkevm-node.
+    node.l1().mine_blocks_periodic(l1_block_time).await;
+
+    // Get test setup from environment.
+    let env = node.env();
+    let l2_provider = env.l2_provider();
+    let mnemonic = env.funded_mnemonic();
+    let hotshot_address = node.l1().hotshot.address();
+
+    let l2 = connect_rpc(&l2_provider, mnemonic, 0, None).await.unwrap();
+    let zkevm = ZkEvm {
+        chain_id: l2.get_chainid().await.unwrap().as_u64(),
+    };
+
+    // Start a sequencer network.
+    let nodes = sequencer::testing::init_hotshot_handles().await;
+    let api_node = nodes[0].clone();
+    let sequencer_store = TempDir::new().unwrap();
+    let options = sequencer::api::Options {
+        port: env.sequencer_port(),
+        storage_path: sequencer_store.path().into(),
+        reset_store: true,
+    };
+
+    sequencer::api::serve(options, Box::new(move |_| ready((api_node, 0)).boxed()))
+        .await
+        .unwrap();
+    for node in nodes {
+        node.start().await;
+    }
+
+    // Start a Polygon zkEVM adaptor.
+    let adaptor_opt = polygon_zkevm_adaptor::Options {
+        l1_provider: env.l1_provider(),
+        sequencer_url: env.sequencer(),
+        rpc_port: env.l2_adaptor_rpc_port(),
+        l2_chain_id: zkevm.chain_id,
+        query_port: env.l2_adaptor_query_port(),
+    };
+    let hotshot_contract_opt = CommitmentTaskOptions {
+        l1_provider: env.l1_provider(),
+        sequencer_mnemonic: mnemonic.to_string(),
+        sequencer_account_index: node.l1().clients.funded[0].index,
+        hotshot_address,
+        l1_chain_id: None,
+        query_service_url: Some(env.sequencer()),
+    };
+    spawn(async move {
+        join!(
+            polygon_zkevm_adaptor::json_rpc::serve(&adaptor_opt),
+            polygon_zkevm_adaptor::query_service::serve(&adaptor_opt),
+            run_hotshot_commitment_task(&hotshot_contract_opt)
+        );
+    });
+
+    (node, sequencer_store)
 }
