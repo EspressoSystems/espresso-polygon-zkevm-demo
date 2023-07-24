e6e941eff809fdb92f19d2e9e569bc1fe241601c
ancient123 <ancients@espressosys.com>
2023-07-24 16:32:05 -0600

    Deal with postgres better
    We were using root credentials for postgres in services.
    - Create root creds that won't be used
    - Don't create database we are using by default
    - Create all users and dbs in init .sql files
    - Set user permissions there as well

--- zkevm-node-additions/init_pool_db.sql
+++ zkevm-node-additions/init_pool_db.sql
@@ -2,3 +2,7 @@
 -- created via the `POSTGRES_DB` environment variable passed to the docker
 -- container.
 CREATE DATABASE pool_db;
+\connect pool_db;
+CREATE USER pool_user with password 'pool_password';
+GRANT ALL ON SCHEMA public TO pool_user;
+GRANT ALL ON DATABASE pool_db TO pool_user WITH GRANT OPTION;
